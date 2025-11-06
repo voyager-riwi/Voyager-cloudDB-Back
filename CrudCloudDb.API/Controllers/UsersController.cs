dbba0b719a4cdc14da3945ed3f27e9e11e909e90
Miguelito <mariasmarin981@gmail.com>
2025-11-06 16:41:16 -0500

    fix(Webhook): The ErrorHandlingMiddleware file was added to handle catch errors in a more general and cleaner way, without adding responsibility to the controllers

--- CrudCloudDb.API/Controllers/UsersController.cs
+++ CrudCloudDb.API/Controllers/UsersController.cs
@@ -4,8 +4,6 @@ using CrudCloudDb.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-
-
 namespace CrudCloudDb.API.Controllers;
 
 [ApiController]
@@ -22,40 +20,24 @@ public class UsersController : ControllerBase
         _userService = userService;
         _webhookService = webhookService;
     }
-
-
-
+    
     [HttpGet("profile")]
     public async Task<IActionResult> GetProfile()
     {
-        try
-        {
-            var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            _logger.LogInformation("Petición recibida para [GET /api/Users/me] por el usuario con ID: {UserId}", userIdString);
-            if (!Guid.TryParse(userIdString, out var userId))
-            {
-                _logger.LogWarning("El token para [GET /api/Users/profile] contenía un ID de usuario con formato inválido: {UserIdString}", userIdString);
-                return Unauthorized("Token invalido");
-            }
-            
-            throw new InvalidOperationException("Error de prueba para notificación en Discord!");
-
-            var result = await _userService.GetProfileAsync(userId);
-            if (result.Succeeded)
-            {
-                return Ok(result);
-            }
-
-            return NotFound();
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e , "Ocurrio un error no controlado en GetMyFrofile");
-            await _webhookService.SendErrorNotificationAsync(e, "Error al intentar obtener el perfil del usuario (GetMyProfile).");
-            return StatusCode(500, new { message = "Ocurrió un error interno en el servidor. El equipo ha sido notificado." });
-        }
         
-            
+        var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        _logger.LogInformation("Petición recibida para [GET /api/Users/me] por el usuario con ID: {UserId}", userIdString);
+        if (!Guid.TryParse(userIdString, out var userId))
+        {
+            _logger.LogWarning("El token para [GET /api/Users/profile] contenía un ID de usuario con formato inválido: {UserIdString}", userIdString);
+            return Unauthorized("Token invalido");
+        }
+        var result = await _userService.GetProfileAsync(userId);
+        if (result.Succeeded)
+        {
+            return Ok(result);
+        }
+        return NotFound();
     }
     
     
@@ -67,25 +49,19 @@ public class UsersController : ControllerBase
             _logger.LogError("Error de validación del modelo para [PUT /api/users/profile]. Errores: {@ModelStateErrors}", ModelState.Values.SelectMany(v => v.Errors));
             return BadRequest(ModelState);
         }
-
         var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        
         _logger.LogDebug("Iniciando actualización de perfil para usuario {UserIdString}. Datos de la petición: {@RequestData}", userIdString, request);
-
         if (!Guid.TryParse(userIdString, out var userId))
         {
             _logger.LogWarning("El token para [PUT /api/users/profile] contenía un ID de usuario con formato inválido: {UserIdString}", userIdString);
             return Unauthorized("Token de usuario inválido.");
         }
-
         var result = await _userService.UpdateProfileAsync(userId, request);
-
         if (result.Succeeded)
         {
             _logger.LogInformation("Perfil del usuario {UserId} actualizado exitosamente.", userId);
             return Ok(result); 
         }
-
         _logger.LogWarning("Fallo en la actualización del perfil para el usuario {UserId}. Razón: {FailureReason}", userId, result.Message);
         return BadRequest(result);
     }
@@ -93,6 +69,7 @@ public class UsersController : ControllerBase
     [HttpPost("change-password")]
     public async Task<IActionResult> ChangeMyPassword([FromBody] ChangePasswordRequestDto request)
     {
+        
         if (!ModelState.IsValid)
         {
             _logger.LogError("Error de validación del modelo para [POST /api/users/profile]. Errores: {@ModelStateErrors}", ModelState.Values.SelectMany(v => v.Errors));
@@ -107,7 +84,7 @@ public class UsersController : ControllerBase
         }
 
         var result = await _userService.ChangePasswordAsync(userId, request);
-
+        
         if (result.Succeeded)
         {
             _logger.LogInformation("Contraseña del usuario {UserId} actualizada exitosamente.", userId);
@@ -116,6 +93,4 @@ public class UsersController : ControllerBase
         _logger.LogWarning("Fallo en la actualización de la contraseña para el usuario {UserId}. Razón: {FailureReason}", userId, result.Message);
         return BadRequest(result);
     }
-    
-    
 }
